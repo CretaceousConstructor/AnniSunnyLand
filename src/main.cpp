36ac2b32390e3d9c1e294b661e9126f2344f9217
Ziyu <theorhythm@gmail.com>
2025-05-19 16:22:09 +0800

    从main分支中，拿到 完成资源管理模块 的提交8e62837的cherry-pick

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 
 #include "engine/core/game_app.h"
+#include <spdlog/spdlog.h>
 
-int main(int, char**)
-{
-	engine::core::GameApp app{};
-	app.run();
-	return 0;
+int main(int /* argc */, char* /* argv */[]) {
+    spdlog::set_level(spdlog::level::debug);
+
+    engine::core::GameApp app;
+    app.run();
+    return 0;
 }
