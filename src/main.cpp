d7004de822697d6d1787f8f69e264a50d57e6725
CretaceousConstructor <397128933@qq.com>
2025-08-30 00:21:37 +0800

    [fix] 修复windows cmd乱码的问题

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 #include "engine/core/game_app.h"
 #include <spdlog/spdlog.h>
 
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#endif // WIN32
+
+
 int main(int /* argc */, char* /* argv */[]) {
+
+#if defined(_WIN32) || defined(_WIN64)
+	SetConsoleOutputCP(CP_UTF8);
+#endif // WIN32
+
     spdlog::set_level(spdlog::level::debug);
 
     engine::core::GameApp app;
